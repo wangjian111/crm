f5c42f260a9bb2d6aaab84c7e3b0c686f1de94af
wangjian111 <2606609713@qq.com>
2019-06-28 21:19:44 +0800

    crm初始化

---
+++ crm/src/main/java/utils/ServiceFactory.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class ServiceFactory {
+	
+	public static Object getService(Object service){
+		
+		return new TransactionInvocationHandler(service).getProxy();
+		
+	}
+	
+}
